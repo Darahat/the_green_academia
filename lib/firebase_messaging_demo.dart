c1f976adc452c7f1da768a66c5527be4110fb42b
didarrahat <didar.rahat@bau.edu.bd>
2020-04-02 10:58:27 +0600

    flutter push notification with firebase complete

--- lib/firebase_messaging_demo.dart
+++ lib/firebase_messaging_demo.dart
@@ -10,18 +10,82 @@ class FirebaseMessagingDemo extends StatefulWidget {
 
 class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
   final FirebaseMessaging _firebaseMessaging= FirebaseMessaging();
+  List<Message> _messages;
   _getToken(){
     _firebaseMessaging.getToken().then((deviceToken){
       print("Device Token: $deviceToken");
     });
   }
-  @override
+ 
+  _configureFirebaseListeners(){
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+        _setMessage(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+        _setMessage(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+        _setMessage(message);
+      },
+    );
+  }
+  _setMessage(Map<String, dynamic> message){
+    final notification = message['notification'];
+    final data= message['data'];
+    final String title = notification['title'];
+    final String body = notification['body'];
+    final String mMessage = data['message'];
+    setState(() {
+      Message m = Message(title,body,mMessage);
+      _messages.add(m);
+    });
+    // Message m = Message(title, body, message);
+    // _messages.add(m);
+
+  }
+   @override
   void initState(){
     super.initState();
+    _messages = List<Message>();
     _getToken();
+    _configureFirebaseListeners();
   }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: null == _messages ? 0 : _messages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(_messages[index].message,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+class Message{
+  String title;
+  String body;
+  String message;
+  Message(title, body,message){
+    this.title = title;
+    this.body = body;
+    this.message = message; 
   }
 }
